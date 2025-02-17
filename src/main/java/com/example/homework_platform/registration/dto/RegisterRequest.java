6a25e1eb12dffc95fc989d9fec3fb622d5799590
ItsmeMarcipan07 <martipan07@abv.bg>
2025-02-17 22:22:03 +0200

    Update RegisterRequest.java

--- src/main/java/com/example/homework_platform/registration/dto/RegisterRequest.java
+++ src/main/java/com/example/homework_platform/registration/dto/RegisterRequest.java
@@ -1,9 +1,19 @@
 package com.example.homework_platform.registration.dto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 
 public class RegisterRequest {
+    @NotBlank(message = "Email cannot be empty")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+            message = "Invalid email format"
+    )
+    public String email;
+
     public String firstName;
     public String lastName;
-    public String email;
     public String password;
     public String role;
     public String phoneNumber;
@@ -12,4 +22,3 @@ public class RegisterRequest {
     public Boolean isClassTeacher;
     public String qualification;
 }
-
