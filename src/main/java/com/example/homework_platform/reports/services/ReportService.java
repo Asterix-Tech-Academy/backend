3f5f42249f588e6c6fd8f44d5e1f791f486826f8
ItsmeMarcipan07 <martipan07@abv.bg>
2025-02-25 22:11:40 +0200

    Add files via upload

---
+++ src/main/java/com/example/homework_platform/reports/services/ReportService.java
@@ -0,0 +1,12 @@
+package com.example.homework_platform.reports.services;
+
+import com.example.homework_platform.reports.models.Report;
+
+import java.util.List;
+
+public interface ReportService {
+    Report getReportById(int id);
+    List<Report> getReportsByAuthor(int authorId);
+    void generateReport(Report report);
+    List<Report> getAllReports();
+}
